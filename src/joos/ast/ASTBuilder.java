93eceb42fa55508c7fe5a306522f14bac261522c
Yi Fei Chen <yf8chen@uwaterloo.ca>
2017-02-01 00:27:48 -0500

    bulit ast for list of elements

--- src/joos/ast/ASTBuilder.java
+++ src/joos/ast/ASTBuilder.java
@@ -2,14 +2,128 @@ package joos.ast;
 
 import joos.commons.ParseTreeNode;
 import joos.commons.ASTTreeNode;
+import joos.commons.TerminalToken;
+import joos.commons.TokenType;
 import joos.exceptions.InvalidSyntaxException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ASTBuilder {
 
 	/**
 	 * Given a parse tree converts it into ast.
 	 */
-	public ASTTreeNode convert(ParseTreeNode parseTree) throws InvalidSyntaxException {
-		return null;
+	public ParseTreeNode convert(ParseTreeNode parseTree) throws InvalidSyntaxException {
+		switch (parseTree.token.getType()) {
+			case MODIFIERS:
+				if (parseTree.children.size() > 1) {
+					ParseTreeNode current = parseTree.children.get(0);
+					while (current.children.size() > 1) {
+						parseTree.children.add(0, current.children.get(1));
+						current = current.children.get(0);
+					}
+					parseTree.children.add(0, current.children.get(0));
+				}
+				break;
+			case FORMAL_PARAMETER_LIST:
+				if (parseTree.children.size() > 1) {
+					ParseTreeNode current = parseTree.children.get(0);
+					while (current.children.size() > 1) {
+						parseTree.children.add(0, current.children.get(2));
+						current = current.children.get(0);
+					}
+					parseTree.children.add(0, current.children.get(0));
+				}
+				break;
+			case IMPORT_DECLARATIONS:
+				if (parseTree.children.size() > 1) {
+					ParseTreeNode current = parseTree.children.get(0);
+					while (current.children.size() > 1) {
+						parseTree.children.add(0, current.children.get(1));
+						current = current.children.get(0);
+					}
+					parseTree.children.add(0, current.children.get(0));
+				}
+				break;
+			case INTERFACE_TYPE_LIST:
+				if (parseTree.children.size() > 1) {
+					ParseTreeNode current = parseTree.children.get(0);
+					while (current.children.size() > 1) {
+						parseTree.children.add(0, current.children.get(2));
+						current = current.children.get(0);
+					}
+					parseTree.children.add(0, current.children.get(0));
+				}
+				break;
+			case CLASS_BODY_DECLARATIONS:
+				if (parseTree.children.size() > 1) {
+					ParseTreeNode current = parseTree.children.get(0);
+					while (current.children.size() > 1) {
+						parseTree.children.add(0, current.children.get(1));
+						current = current.children.get(0);
+					}
+					parseTree.children.add(0, current.children.get(0));
+				}
+				break;
+			case CLASS_TYPE_LIST:
+				if (parseTree.children.size() > 1) {
+					ParseTreeNode current = parseTree.children.get(0);
+					while (current.children.size() > 1) {
+						parseTree.children.add(0, current.children.get(2));
+						current = current.children.get(0);
+					}
+					parseTree.children.add(0, current.children.get(0));
+				}
+				break;
+			case INTERFACE_MEMBER_DECLARATIONS:
+				if (parseTree.children.size() > 1) {
+					ParseTreeNode current = parseTree.children.get(0);
+					while (current.children.size() > 1) {
+						parseTree.children.add(0, current.children.get(1));
+						current = current.children.get(0);
+					}
+					parseTree.children.add(0, current.children.get(0));
+				}
+				break;
+			case VARIABLE_INITIALIZERS:
+				if (parseTree.children.size() > 1) {
+					ParseTreeNode current = parseTree.children.get(0);
+					while (current.children.size() > 1) {
+						parseTree.children.add(0, current.children.get(2));
+						current = current.children.get(0);
+					}
+					parseTree.children.add(0, current.children.get(0));
+				}
+				break;
+			case BLOCK_STATEMENTS:
+				if (parseTree.children.size() > 1) {
+					ParseTreeNode current = parseTree.children.get(0);
+					while (current.children.size() > 1) {
+						parseTree.children.add(0, current.children.get(1));
+						current = current.children.get(0);
+
+					}
+					parseTree.children.add(0, current.children.get(0));
+				}
+				break;
+			case STATEMENT_EXPRESSION_LIST:
+				if (parseTree.children.size() > 1) {
+					ParseTreeNode current = parseTree.children.get(0);
+					while (current.children.size() > 1) {
+						parseTree.children.add(0, current.children.get(2));
+						current = current.children.get(0);
+					}
+					parseTree.children.add(0, current.children.get(0));
+				}
+				break;
+			default:
+		}
+		if (parseTree.children != null) {
+			for (ParseTreeNode child : parseTree.children) {
+				convert(child);
+			}
+		}
+		return parseTree;
 	}
 }

8e9bf079a399d1b5dd7c34f9c1d06c313f88387a
1habeebahmed1 <1habeeb.ahmed1@gmail.com>
2017-03-10 22:59:41 -0500

    asdasd

--- src/joos/environment/Environment.java
+++ src/joos/environment/Environment.java
@@ -75,21 +75,25 @@ public class Environment {
 	public MethodSignature findMethodSignature(Map<String, Environment> packageMap, MethodSignature partialSignature) throws InvalidSyntaxException {
 		if (mName.equals(partialSignature.name)) {
 			List<String> constructorParamTypes = getConstructorSignature(packageMap);
-			if(constructorParamTypes.equals(partialSignature.parameterTypes)) {
-				return new MethodSignature(
-					partialSignature.name,
-					partialSignature.name,
-					partialSignature.parameterTypes,
-					null,
-					null);
+			if (constructorParamTypes != null) {
+				if(constructorParamTypes.equals(partialSignature.parameterTypes)) {
+					return new MethodSignature(
+						partialSignature.name,
+						partialSignature.name,
+						partialSignature.parameterTypes,
+						null,
+						null);
+				}
 			}
 		}
 		Map<String, Map<List<String>, MethodSignature>> methodSignatures = getMethodSignatures(packageMap);	
-		Map<List<String>, MethodSignature> things = methodSignatures.get(partialSignature.name);
-		if (things != null) {
-			MethodSignature methSig = things.get(partialSignature.parameterTypes);
-			if(methSig != null) {
-				return methSig;
+		if (methodSignatures != null) {
+			Map<List<String>, MethodSignature> things = methodSignatures.get(partialSignature.name);
+			if (things != null) {
+				MethodSignature methSig = things.get(partialSignature.parameterTypes);
+				if(methSig != null) {
+					return methSig;
+				}
 			}
 		}
 		List<Environment> extendedEnvironments = EnvironmentUtils.getExtendedEnvironments(this, packageMap);

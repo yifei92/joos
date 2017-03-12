8c88f46d119a253690e085c4c1c818d69d2a40f4
Steven McConomy <srmccono@uwaterloo.ca>
2017-03-12 16:42:21 -0400

    asdf

--- src/joos/environment/EnvironmentUtils.java
+++ src/joos/environment/EnvironmentUtils.java
@@ -462,9 +462,9 @@ public class EnvironmentUtils {
 		if (isArray.ref) {
 			Type subType;
 			if (isPrimitive.ref) {
-				subType = Type.newPrimitive(typeName, node);
+				subType = Type.newPrimitive(typeName + "[]", node);
 			} else {
-				subType = Type.newObject(typeName, getEnvironmentFromTypeName(environment, typeName, packageMap), node);
+				subType = Type.newObject(typeName + "[]", getEnvironmentFromTypeName(environment, typeName, packageMap), node);
 			}
 			return Type.newArray(typeName, subType, node);
 		} else {

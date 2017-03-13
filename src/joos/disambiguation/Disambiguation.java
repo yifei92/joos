4a4d648ec2e54f8b207e1f809d42c5a907808e23
Steven McConomy <srmccono@uwaterloo.ca>
2017-03-12 22:57:33 -0400

    asdf

--- src/joos/disambiguation/Disambiguation.java
+++ src/joos/disambiguation/Disambiguation.java
@@ -321,7 +321,6 @@ public class Disambiguation {
         }
       }
     }
-    if (environment.mVariableDeclarations.containsKey(prefix))System.out.println(environment.mVariableDeclarations.get(prefix).isBefore(node));
     switch (getEnvironmentType(environment)) {
       case CLASS:
         for (Environment extendedEnvironment : getExtendedEnvironments(environment, packageMap)) {

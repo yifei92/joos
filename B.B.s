5c05e11d7145af98cb666556874850c13998a141
1habeebahmed1 <1habeeb.ahmed1@gmail.com>
2017-03-22 19:12:26 -0400

    asd

--- B.B.s
+++
@@ -1,9 +0,0 @@
-global VTABLE$B.B
-VTABLE$B.B:
-  dd METHOD$java.lang.Object$equals@java.lang.Object#
-  dd METHOD$java.lang.Object$toString@
-  dd METHOD$java.lang.Object$hashCode@
-  dd METHOD$java.lang.Object$clone@
-  dd METHOD$java.lang.Object$getClass@
-  dd METHOD$A.A$instanceMethod@
-
--- java.lang.Class.s
+++
@@ -1,8 +0,0 @@
-global VTABLE$java.lang.Class
-VTABLE$java.lang.Class:
-  dd METHOD$java.lang.Object$equals@java.lang.Object#
-  dd METHOD$java.lang.Object$toString@
-  dd METHOD$java.lang.Object$hashCode@
-  dd METHOD$java.lang.Object$clone@
-  dd METHOD$java.lang.Object$getClass@
-
